9c07a5a69370cc86ceaf4f343ccd79997d981e28
OlhaShyliaieva <olshilyaeva@gmail.com>
2018-10-25 11:51:11 +0300

    minor fix

--- rg_instructor_analytics_log_collector/admin.py
+++ rg_instructor_analytics_log_collector/admin.py
@@ -11,12 +11,13 @@ class LastProcessedLogAdmin(admin.ModelAdmin):
     """
     Django admin customizations for LastProcessedLog model.
     """
+
     list_display = ('processor', 'log_time',)
     raw_id_fields = ('log_table',)
 
     def log_time(self, obj):
         """
-        Customizations for LastProcessedLog model fields.
+        Format for LastProcessedLog field.
         """
         return obj.log_table.log_time
 
