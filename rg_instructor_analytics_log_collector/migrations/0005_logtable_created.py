e7554b3f400ca4abf9a7016e32424f35cd7c6b01
OlhaShyliaieva <olshilyaeva@gmail.com>
2018-10-31 14:10:01 +0200

    Fix selection LogTable and add log info in processors

---
+++ rg_instructor_analytics_log_collector/migrations/0005_logtable_created.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('rg_instructor_analytics_log_collector', '0004_lastprocessedlog'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='logtable',
+            name='created',
+            field=models.DateTimeField(default=datetime.datetime(2018, 10, 31, 12, 8, 10, 255604, tzinfo=utc), auto_now_add=True),
+            preserve_default=False,
+        ),
+    ]
