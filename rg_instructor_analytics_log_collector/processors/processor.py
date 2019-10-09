ff7b8359ff4c19c86e9c86efb87bd5014a564118
Maxim Starodubcev <maxim.starodubcev@raccoongang.com>
2019-10-09 17:07:00 +0300

    Optimize log processor

--- rg_instructor_analytics_log_collector/processors/processor.py
+++ rg_instructor_analytics_log_collector/processors/processor.py
@@ -7,6 +7,8 @@ import time
 
 from rg_instructor_analytics_log_collector.models import LastProcessedLog, LogTable
 from rg_instructor_analytics_log_collector.processors.base_pipeline import EnrollmentPipeline
+from django.db.models import Min, Max
+from django.db import transaction
 
 log = logging.getLogger(__name__)
 
@@ -19,6 +21,9 @@ class Processor(object):
     available_pipelines = [
         EnrollmentPipeline()
     ]
+
+    CHUNK_SIZE_PROCESSOR = 10000
+    CHUNK_SIZE_DELETE = 50000
 
     def __init__(self, alias_list, sleep_time):
         """
@@ -46,9 +51,23 @@ class Processor(object):
     def delete_logs(self):
         """Delete all unused log records."""
         last_date = LastProcessedLog.get_last_date()
+
         if last_date:
-            logging.info('Deleting old log records...')
-            LogTable.objects.filter(log_time__lt=last_date).delete()
+            records = LogTable.objects.filter(log_time__lt=last_date).order_by('log_time')
+            chunk_size = self.CHUNK_SIZE_DELETE
+
+            while records.exists():
+                if records.count() > chunk_size:
+                    delete_max_time = records[chunk_size].log_time
+                else:
+                    delete_max_time = last_date
+
+                logging.info('Deleting log records older than {}'.format(delete_max_time))
+
+                with transaction.atomic():
+                    LogTable.objects.filter(log_time__lt=delete_max_time).delete()
+
+                records = LogTable.objects.filter(log_time__lt=last_date).order_by('log_time')
 
     def run(self, delete_logs=False):
         """
@@ -56,21 +75,41 @@ class Processor(object):
         """
         while True:
             for pipeline in self.pipelinies:
-                logging.info('{} processor started at {}'.format(pipeline.alias, datetime.now()))
                 records = self._get_query_for_pipeline(pipeline)
                 last_record = records.last()
 
-                if not records:
-                    logging.info('{} processor stopped at {} (no records)'.format(pipeline.alias, datetime.now()))
+                if not records.exists():
+                    logging.debug('{} processor stopped at {} (no records)'.format(pipeline.alias, datetime.now()))
                     continue
 
-                records = filter(None, [pipeline.format(record) for record in records])
+                time_start = datetime.now()
+                logging.info('{} processor started at {}'.format(pipeline.alias, time_start))
 
-                for record in records:
-                    pipeline.push_to_database(record)
+                chunk_size = self.CHUNK_SIZE_PROCESSOR
+                records_counter = 0
+                records_pushed_counter = 0
+                records_count = records.count()
 
-                pipeline.update_last_processed_log(last_record)
-                logging.info('{} processor stopped at {}'.format(pipeline.alias, datetime.now()))
+                for offset in range(0, records_count, chunk_size):
+
+                    logging.info('{}: total records: {}. processing from {} to {}'.format(
+                                pipeline.alias,records_count,offset,offset+chunk_size))
+
+                    for record in records[offset:offset+chunk_size]:
+                        data_record = pipeline.format(record)
+                        records_counter += 1
+
+                        if data_record:
+                            pipeline.push_to_database(data_record)
+                            records_pushed_counter += 1
+                        pipeline.update_last_processed_log(record)
+
+                logging.info(
+                    '{} processor stopped at {} (processed: {}, saved: {}, rate: {} rps)'.format(
+                        pipeline.alias, datetime.now(), records_counter, records_pushed_counter,
+                        int(records_counter / (datetime.now() - time_start).total_seconds())
+                    )
+                )
 
             if delete_logs:
                 self.delete_logs()
